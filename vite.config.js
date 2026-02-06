15d4fa6140a08cb810e24bdd92d3a281c39ebb9e
John Mitchell <webbywiz@gmail.com>
2026-02-06 18:32:28 -0500

    Update Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/LakelandTrek25/",   // 👈 VERY IMPORTANT
+  base: "/crowdsnap/",   // IMPORTANT
 });
+
