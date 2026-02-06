1bbd25439b24bc801efc72ed9fb919eb4c4b32f7
John Mitchell <webbywiz@gmail.com>
2026-02-06 18:35:32 -0500

    Add dev env templates and workspace settings

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/LakelandTrek25">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 )
+
